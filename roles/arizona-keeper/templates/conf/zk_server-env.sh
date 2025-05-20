e356d0130c877158f3d6d8144858758fbabd7c34
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-05-20 08:07:50 -0400

    Halfofa zookeeper

---
+++ roles/arizona-keeper/templates/conf/zk_server-env.sh
@@ -0,0 +1,14 @@
+ZK_KEYSTORE_LOCATION={{ keystore_location }}
+ZK_KEYSTORE_PASSWORD={{ keystore_password }}
+ZK_TRUSTSTORE_LOCATION={{ truststore_location }}
+ZK_TRUSTSTORE_PASSWORD={{ truststore_password }}
+
+SSL_JVM_FLAGS="-Dzookeeper.serverCnxnFactory=org.apache.zookeeper.server.NettyServerCnxnFactory \
+-Dzookeeper.ssl.keyStore.location=$ZK_KEYSTORE_LOCATION \
+-Dzookeeper.ssl.keyStore.password=$ZK_KEYSTORE_PASSWORD \
+-Dzookeeper.ssl.trustStore.location=$ZK_TRUSTSTORE_LOCATION \
+-Dzookeeper.ssl.trustStore.password=$ZK_TRUSTORE_PASSWORD \
+-Dzookeeper.ssl.endpoint.identification.algorithm=false "
+
+export SERVER_JVMFLAGS="${SERVER_JVMFLAGS} ${SSL_JVM_FLAGS}"
+
