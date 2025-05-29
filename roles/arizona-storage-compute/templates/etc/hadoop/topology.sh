4eb3df0f789c74ddf258df05cf63da251edd1380
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-05-29 16:06:02 -0400

    Refinements

--- roles/arizona-storage-compute/templates/etc/hadoop/topology.sh
+++ roles/arizona-storage-compute/templates/etc/hadoop/topology.sh
@@ -4,7 +4,9 @@ while [ $# -gt 0 ] ; do
   NARG=$1
   exec< ${HADOOP_CONF}/topology.data
   RES=""
-  while read line ;do
+  while read line ; do
+    #THERE IS ALL types of nastness with space andtab especially because jina might turn a tab into 4 spaces. | is a better choice
+    IFS='|'
     AR=( $line )
     if [ "${AR[0]}" = "$NARG" ]
     then
