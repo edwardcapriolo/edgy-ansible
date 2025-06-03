322e72717a658197c49a9f35e407191bd2f6b7f5
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-06-03 08:42:32 -0400

    Checkpoint

--- examples/install-keeper.sh
+++ examples/install-keeper.sh
@@ -1,1 +1,2 @@
 ansible-playbook arizona_keeper_local.yml -i hosts/LOCAL/hosts --extra-vars "operation=install hostname=arizona_keeper"
+ansible-playbook arizona_keeper_local.yml -i hosts/LOCAL/hosts --extra-vars "operation=localize hostname=arizona_keeper"
--- roles/arizona-keeper/templates/bin/arizona-keeper-zkServer.sh
+++ roles/arizona-keeper/templates/bin/arizona-keeper-zkServer.sh
@@ -1,2 +1,3 @@
+pushd "{{ apache_zookeeper_home }}/apache-zookeeper-{{ apache_zookeeper_version }}-bin/conf"
 source ./arizona-keeper-server-env.sh
 ./zkServer.sh "$@"
