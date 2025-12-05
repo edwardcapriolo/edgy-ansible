e44ed7c2cbfc3f52c0425e63409d8068821e493b
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-12-05 11:21:30 -0500

    Push it to the hub

--- imaging/tiny-spark/push.sh
+++ imaging/tiny-spark/push.sh
@@ -1,3 +1,3 @@
-docker tag tiny-spark:latest ecapriolo/tiny-spark:3.5.7-1
-docker push ecapriolo/tiny-spark:3.5.7-1
+docker tag tiny-spark:latest ecapriolo/tiny-spark:3.5.7-2
+docker push ecapriolo/tiny-spark:3.5.7-2
 
