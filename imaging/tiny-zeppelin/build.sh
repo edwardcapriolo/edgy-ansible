ba0fa1e41713cb6004d32d5c7a6282df79fdaf0b
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-12-12 13:39:14 -0500

    a build script

--- imaging/tiny-zeppelin/build.sh
+++ imaging/tiny-zeppelin/build.sh
@@ -5,24 +5,27 @@ if [ ! -e "zeppelin-0.12.0-bin-all.tgz" ]; then
   tar -xf zeppelin-0.12.0-bin-all.tgz
 fi
 
-rm zeppelin-0.12.0-bin-all/interpreter/alluxio
-rm zeppelin-0.12.0-bin-all/interpreter/flink
-rm zeppelin-0.12.0-bin-all/interpreter/flink-cmd
-rm zeppelin-0.12.0-bin-all/interpreter/influxdb
+rm -rf zeppelin-0.12.0-bin-all/interpreter/alluxio
+rm -rf zeppelin-0.12.0-bin-all/interpreter/flink
+rm -rf zeppelin-0.12.0-bin-all/interpreter/flink-cmd
+rm -rf zeppelin-0.12.0-bin-all/interpreter/influxdb
 #show me your not a nosql fanboy
-rm zeppelin-0.12.0-bin-all/interpreter/mongodb
-rm zeppelin-0.12.0-bin-all/interpreter/neo4j
-rm zeppelin-0.12.0-bin-all/interpreter/hbase
-rm zeppelin-0.12.0-bin-all/interpreter/neo4j
+rm -rf zeppelin-0.12.0-bin-all/interpreter/mongodb
+rm -rf zeppelin-0.12.0-bin-all/interpreter/neo4j
+rm -rf zeppelin-0.12.0-bin-all/interpreter/hbase
+rm -rf zeppelin-0.12.0-bin-all/interpreter/neo4j
 
-rm zeppelin-0.12.0-bin-all/interpreter/r
-rm zeppelin-0.12.0-bin-all/interpreter/sparql
+rm -rf zeppelin-0.12.0-bin-all/interpreter/r
+rm -rf zeppelin-0.12.0-bin-all/interpreter/sparql
 
 
 cat << EOF > Dockerfile
 
 FROM ecapriolo/jre-17:0.0.1 AS zeppelin
 RUN apk add --no-cache bash
+RUN apk add --no-cache python3 
+
+# py3-pip
 COPY zeppelin-0.12.0-bin-all /opt/zeppelin 
 WORKDIR /opt/zeppelin
 ENTRYPOINT ["/opt/zeppelin/bin/zeppelin.sh"]
@@ -35,9 +38,11 @@ EOF
 
 
 docker build \
+--no-cache \
 --target zeppelin \
 -t tiny-zeppelin .
 
 docker build \
+--no-cache \
 --target zeppelin-and-tiny-spark \
 -t tiny-zeppelin-and-tiny-spark .
