0effa3ad237ecbb964b3a152d807d928717175d5
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-12-14 08:36:57 -0500

    Push script

---
+++ imaging/tiny-zeppelin/push.sh
@@ -0,0 +1,7 @@
+docker tag tiny-zeppelin:latest ecapriolo/tiny-zeppelin:0.12.0-1
+docker push ecapriolo/tiny-zeppelin:0.12.0-1
+
+
+docker tag tiny-zeppelin-and-tiny-spark:latest ecapriolo/tiny-zeppelin-and-tiny-spark:0.12.0-1
+docker push ecapriolo/tiny-zeppelin-and-tiny-spark:0.12.0-1
+
