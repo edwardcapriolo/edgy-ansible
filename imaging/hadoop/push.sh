8380e3cf42fba6052ad03ce48418a956339b38cb
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-12-23 16:41:06 -0500

    Some stuff

--- imaging/hadoop/push.sh
+++ imaging/hadoop/push.sh
@@ -1,11 +1,11 @@
-docker tag tiny-hadoop:latest ecapriolo/tiny-hadoop:3.4.2-3
-docker push ecapriolo/tiny-hadoop:3.4.2-3
+docker tag tiny-hadoop:latest ecapriolo/tiny-hadoop:3.4.2-4
+docker push ecapriolo/tiny-hadoop:3.4.2-4
 
-docker tag tiny-yarn:latest ecapriolo/tiny-yarn:3.4.2-3
-docker push ecapriolo/tiny-yarn:3.4.2-3
+docker tag tiny-yarn:latest ecapriolo/tiny-yarn:3.4.2-4
+docker push ecapriolo/tiny-yarn:3.4.2-4
 
-docker tag tiny-hdfs:latest ecapriolo/tiny-hdfs:3.4.2-3
-docker push ecapriolo/tiny-hdfs:3.4.2-3
+docker tag tiny-hdfs:latest ecapriolo/tiny-hdfs:3.4.2-4
+docker push ecapriolo/tiny-hdfs:3.4.2-4
 
 
 
