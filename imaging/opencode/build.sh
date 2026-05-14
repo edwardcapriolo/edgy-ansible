46c5a59101ed7e67300ffe59c6f7d78077bb9597
Edward Guy Capriolo <edlinuxguru@gmail.com>
2026-05-14 00:07:08 +0000

    Update

--- imaging/opencode/build.sh
+++ imaging/opencode/build.sh
@@ -1,9 +1,9 @@
 
 cat << EOF > Dockerfile
-FROM jdk-25-cdevel AS trusted-opencode 
+FROM jdk-25-cdevel AS trusted-opencode-build 
 
-RUN apk add  --no-cache  go npm python3
-
+RUN apk --no-cache  add go npm python3
+RUN apk --no-cache  add gcc g++ make
 #RUN apk add gcompat libstdc++ libgcc
 
 RUN addgroup -S opencode && adduser -S -G opencode -h /opencode -D opencode
@@ -16,14 +16,8 @@ COPY --from=oven/bun:alpine /usr/local/bin/bun /usr/local/bin/bun
 RUN git clone https://github.com/anomalyco/opencode.git
 RUN cd opencode
 WORKDIR /opencode/opencode
-#RUN bun init
 RUN bun install
-RUN bun run build
-##RUN go mod download 
-##RUN go build -o opencode ./cmd/opencode
-
-# no web ui
-#RUN go install github.com/opencode-ai/opencode@latest
+RUN ./packages/opencode/script/build.ts --single
 
 USER root
 RUN addgroup -S acoder && adduser -S -G acoder -h /home/acoder -D acoder
@@ -31,8 +25,22 @@ RUN addgroup -S acoder && adduser -S -G acoder -h /home/acoder -D acoder
 WORKDIR /home/acoder
 USER acoder
 
+FROM jdk-25-cdevel AS trusted-opencode
+
+RUN addgroup -S acoder && adduser -S -G acoder -h /home/acoder -D acoder
+
+COPY --from=trusted-opencode-build /opencode/opencode/packages/opencode/dist/opencode-linux-x64/bin/opencode /usr/local/bin/opencode
+
+WORKDIR /home/acoder                                                    
+USER acoder
+
+
 EOF
 
 docker build \
--t trusted-opencode .
+--target trusted-opencode-build \
+-t trusted-opencode-build .
     
+docker build \
+--target trusted-opencode \
+-t trusted-opencode .
